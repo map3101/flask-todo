54c7cd501f550c4c7f6412c8fd06ccd1b38b0db4
map3101 <marcelo.postiglione@gmail.com>
2022-01-28 18:11:39 -0300

    Feat: Altera ordem das tarefas retornadas das mais recentes para as mais antigas

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
         except:
             return 'ERROR'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = Todo.query.order_by(Todo.date_created.desc()).all()
         return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
